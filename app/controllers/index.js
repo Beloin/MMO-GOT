87253719b56e51b3aff39163c346a30c86ad19fa
Beloin Sena <beloin.rodrigues@gmail.com>
2020-07-10 08:53:29 -0300

    Adicionado cadastro

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function (application, req, res) {
-    res.render("index");
+    res.render("index", { validacao: {} });
+};
+
+module.exports.autenticar = function (application, req, res) {
+    const dados = req.body;
+
+    req.assert("usuario", "Usuário não pode ser vazio").notEmpty();
+    req.assert("senha", "Senha não pode ser vazia").notEmpty();
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        res.render("index", { validacao: errors });
+        return;
+    }
+
+    res.send("Tudo ok para criar a sessão");
 };
